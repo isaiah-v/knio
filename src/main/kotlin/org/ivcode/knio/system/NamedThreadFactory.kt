e6b948220da7fe91f1db45a0c9c5c7106a93787d
isaiah.v <isaiah.v@comcast.net>
2025-01-09 02:17:05 -0800

    Cleanup. Added the ability to define the dispatcher in the "use method"

--- src/main/kotlin/org/ivcode/knio/system/NamedThreadFactory.kt
+++ src/main/kotlin/org/ivcode/knio/system/NamedThreadFactory.kt
@@ -3,15 +3,16 @@ package org.ivcode.org.ivcode.knio.system
 import java.util.concurrent.ThreadFactory
 import java.util.concurrent.atomic.AtomicLong
 
-class NamedThreadFactory(
+class NamedVirtualThreadFactory(
     private val name: String
 ): ThreadFactory {
 
     private val counter = AtomicLong(1)
 
     override fun newThread(r: Runnable): Thread {
-        val thread = Thread(r)
+        val thread = Thread.ofVirtual().unstarted(r)
         thread.name = "$name-${counter.getAndIncrement()}"
+
         return thread
     }
 }
